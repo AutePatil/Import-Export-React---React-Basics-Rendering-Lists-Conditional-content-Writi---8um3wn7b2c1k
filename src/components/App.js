203867686dae3d5ec08035f2852e877d00f1fda3
AutePatil <sagaraute7@gmail.com>
2022-11-03 22:14:20 +0530

    import export changes in App.js & Heading.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import {Heading} from './Heading';
 
 const App = () => {
   return (
     <div id="main">
-      < />
+      < Heading/>
       < />
       < />
       < />
